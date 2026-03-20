57313abab16741bbb53ab4c1cd71034b44cdb121
богдан петропавловский <doganddog23@gmail.com>
2026-03-20 09:19:44 +0300

    Очистка Дубны 2.0

--- 03_reset_today_for_location.sql
+++ 03_reset_today_for_location.sql
@@ -3,19 +3,19 @@
 BEGIN TRANSACTION;
 
 DELETE FROM verify_attempt_progress
-WHERE report_id = 6;
+WHERE report_id = 4;
 
 DELETE FROM check_results
-WHERE report_id = 6;
+WHERE report_id = 4;
 
 DELETE FROM report_target_snapshots
-WHERE report_id = 6;
+WHERE report_id = 4;
 
 DELETE FROM report_employee_completions
-WHERE report_id = 6;
+WHERE report_id = 4;
 
 DELETE FROM category_assignments
-WHERE location = 'Дмитров'
+WHERE location = 'Дубна'
   AND cycle_version = 1;
 
 COMMIT;
