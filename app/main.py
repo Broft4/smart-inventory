7cb7ce06ebfddc1774a4e8863faabe06eda3eeda
богдан петропавловский <doganddog23@gmail.com>
2026-06-09 23:31:44 +0300

    2.2.3.3 - Отредактирована темная тема 4

--- app/main.py
+++ app/main.py
@@ -247,7 +247,7 @@ app.add_middleware(
 
 app.mount('/static', StaticFiles(directory=BASE_DIR / 'static'), name='static')
 templates = Jinja2Templates(directory=str(BASE_DIR / 'templates'))
-templates.env.globals['asset_version'] = '20260609-mobile-payroll-status-v1'
+templates.env.globals['asset_version'] = '20260609-ios-admin-fields-v1'
 
 
 @app.middleware('http')
